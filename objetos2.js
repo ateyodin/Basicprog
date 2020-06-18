8e09bcc8ae05ac96a6ad9eb08f32dd0e49a4e0f5
ateyodin <52425179+ateyodin@users.noreply.github.com>
2020-06-17 21:22:06 -0400

    Add files via upload

---
+++ objetos2.js
@@ -0,0 +1,106 @@
+//objetos 5
+
+var andres = 
+{
+    nombre : "Andres",
+    apellido : "Ramirez",
+    edad : 32,
+}
+
+var david = 
+{
+    nombre : "David",
+    apellido : "Ramirez",
+    edad : 27,
+}
+
+function imprimirEnMayusculas(persona)
+{
+    var {nombre} = persona;  // var nombre = persona.nombre
+    console.log(nombre.toUpperCase());
+}
+
+
+imprimirEnMayusculas(andres);
+imprimirEnMayusculas(david);
+imprimirEnMayusculas({nombre:"pepito"});
+
+//objetos 6
+
+var andres = 
+{
+    nombre : "Andres",
+    apellido : "Ramirez",
+    edad : 32,
+}
+
+var david = 
+{
+    nombre : "David",
+    apellido : "Ramirez",
+    edad : 27,
+}
+
+function imprimirNombreYEdad(persona)
+{
+    var {nombre} = persona;  // var nombre = persona.nombre
+    var {edad} = persona
+    console.log(`Hola me llamo ${nombre} y tengo ${edad}`);
+}
+
+
+imprimirNombreYEdad(andres);
+imprimirNombreYEdad(david);
+imprimirNombreYEdad({nombre:"pepito", edad: 28});
+
+// objetos 7
+
+var andres = 
+{
+    nombre : "Andres",
+    apellido : "Ramirez",
+    edad : 32,
+}
+
+var david = 
+{
+    nombre : "David",
+    apellido : "Ramirez",
+    edad : 27,
+}
+
+function cumpleanos(persona)
+{
+    persona.edad += 1
+    console.log(persona.edad)
+}
+
+cumpleanos(andres)
+
+//objetos 8
+
+var andres = 
+{
+    nombre : "Andres",
+    apellido : "Ramirez",
+    edad : 32,
+}
+
+var david = 
+{
+    nombre : "David",
+    apellido : "Ramirez",
+    edad : 27,
+}
+
+function cumpleanos(persona)
+{
+    return
+    {
+        ...persona,
+        edad: persona.edad + 1
+
+    }
+}
+
+cumpleanos(andres)
