2544514617140184807bd6e7a13d7f662f0c64f7
ateyodin <52425179+ateyodin@users.noreply.github.com>
2020-06-17 20:49:52 -0400

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,114 @@
+var nombreAndres = "Andres";
+var nombreDario = "Dario";
+
+function imprimirEnMayusculas(n)
+{
+    n = n.toUpperCase();
+    console.log(n);
+}
+
+imprimirEnMayusculas(nombreAndres);
+imprimirEnMayusculas(nombreDario);
+
+// Objetos
+
+var andres = 
+{
+    nombre : "Andres",
+    apellido : "Ramirez",
+    edad : 32,
+}
+
+var david = 
+{
+    nombre : "David",
+    apellido : "Ramirez",
+    edad : 27,
+}
+
+function imprimirEnMayusculas(n)
+{
+    n = n.toUpperCase();
+    console.log(n);
+}
+
+
+imprimirEnMayusculas(andres.nombre);
+imprimirEnMayusculas(david.nombre);
+
+// objetos 2
+
+var andres = 
+{
+    nombre : "Andres",
+    apellido : "Ramirez",
+    edad : 32,
+}
+
+var david = 
+{
+    nombre : "David",
+    apellido : "Ramirez",
+    edad : 27,
+}
+
+function imprimirEnMayusculas(persona)
+{
+    var nombre = persona.nombre.toUpperCase();
+    console.log(nombre);
+}
+
+
+imprimirEnMayusculas(andres);
+imprimirEnMayusculas(david);
+
+// objetos 3
+
+var andres = 
+{
+    nombre : "Andres",
+    apellido : "Ramirez",
+    edad : 32,
+}
+
+var david = 
+{
+    nombre : "David",
+    apellido : "Ramirez",
+    edad : 27,
+}
+
+function imprimirEnMayusculas(persona)
+{
+    console.log(persona.nombre.toUpperCase());
+}
+
+
+imprimirEnMayusculas(andres);
+imprimirEnMayusculas(david);
+
+// objetos 4
+
+var andres = 
+{
+    nombre : "Andres",
+    apellido : "Ramirez",
+    edad : 32,
+}
+
+var david = 
+{
+    nombre : "David",
+    apellido : "Ramirez",
+    edad : 27,
+}
+
+function imprimirEnMayusculas({nombre})
+{
+    console.log(nombre.toUpperCase());
+}
+
+
+imprimirEnMayusculas(andres);
+imprimirEnMayusculas(david);
+imprimirEnMayusculas({nombre:"pepito"});
